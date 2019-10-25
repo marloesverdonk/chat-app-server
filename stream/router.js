054ae9c6b2198dd09b3c36fcd41ae4edc0be7f73
marloesverdonk <marloesverdonk@live.nl>
2019-10-25 11:26:56 +0200

    You can post messages now

--- stream/router.js
+++ stream/router.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const Chatroom = require('./model')
 
 const router = new Router()
 
@@ -7,5 +8,21 @@ router.get('/stream', (req, res, next) => {
   res.status(200)
   res.send('works')
 })
+
+router.post('/message', (req, res, next) => {
+  console.log('request body:', req.body)
+  Chatroom.create(req.body)
+    .then(chatroom => res.json(chatroom))
+    .catch(next)
+})
+
+// router.post('/message', async (req, res, next) => {
+//  const {message} = req.body
+//  const entity = await Chatroom.create({
+//    message: message
+//  })
+//  res.status(201)
+//  res.send(entity)
+// })
 
 module.exports = router
